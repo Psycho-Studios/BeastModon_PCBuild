ba673a278967fc5a1af21c7bf69f061e39f9165f
Jeshawn <jeshawnchrite@gmail.com>
2022-07-14 10:45:32 -0400

    Sprites were corrupted after following poor advice to delete the .meta files. Will slice and implement again. Facial animations 70% done.

--- Assets/HUD_Player1.cs
+++ Assets/HUD_Player1.cs
@@ -4,56 +4,68 @@ using UnityEngine;
 
 public class HUD_Player1 : MonoBehaviour
 {
+    private bool bool_initialAnimationsCompleted;
     public static bool bool_rankUpRequest_player1, bool_beastModeRequest_player1,
         bool_restoreWeaponRequest;
     public GameObject[] gameObjects_weaponSlots;
-    private Animator[] animators_weaponSlots;
+    private List<Animator> animators_weaponSlots;
     
     // Start is called before the first frame update
     private void Awake()
     {
         bool_rankUpRequest_player1 = false;
-        animators_weaponSlots = new Animator[5];
-        for(int i = 0; i < 5; i++)
+        animators_weaponSlots = new List<Animator>();
+        for (int i = 0; i < 5; i++)
         {
-            animators_weaponSlots[i] = gameObjects_weaponSlots[i].GetComponent<Animator>();
+            animators_weaponSlots.Add(gameObjects_weaponSlots[i].GetComponent<Animator>());
         }
     }
 
     private void Start()
     {
-        for (int i = 0; i < 5; i++)
-        {
-            animators_weaponSlots[i].SetInteger
-               ("WeaponValue",
-               i + (5 * ProjectileControls.array_int_weaponStrength[i]));
-        }
+        bool_initialAnimationsCompleted = false;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        if (bool_rankUpRequest_player1
-        && !bool_beastModeRequest_player1)
+        if (ProjectileControls_Player1.bool_weaponStrengthArrayPopulated
+        && !bool_initialAnimationsCompleted)
         {
-            animators_weaponSlots[ProjectileControls.int_weaponIndex_player1].SetInteger
-                ("WeaponValue",
-                ProjectileControls.int_weaponValue_player1);
-            bool_rankUpRequest_player1 = false;
+            for (int i = 0; i < 5; i++)
+            {
+                animators_weaponSlots[i].SetInteger(
+                "WeaponValue",
+                i + (5 * ProjectileControls_Player1.array_int_weaponStrength[i]));
+            }
+            bool_initialAnimationsCompleted = true;
         }
-        if(bool_beastModeRequest_player1)
-        {
-            animators_weaponSlots[ProjectileControls.int_weaponIndex_player1].SetInteger
-                ("WeaponValue",
-                ProjectileControls.int_weaponValue_player1);
-            bool_beastModeRequest_player1 = false;
-        }
-        if(bool_restoreWeaponRequest)
-        {
-            animators_weaponSlots[ProjectileControls.int_weaponIndex_player1].SetInteger
-                ("WeaponValue",
-                ProjectileControls.int_weaponValue_player1);
-            bool_restoreWeaponRequest = false;
+
+        if(bool_initialAnimationsCompleted)
+        { 
+            //Animate the weapon slots based on weapon type
+            if (bool_rankUpRequest_player1
+            && !bool_beastModeRequest_player1)
+            {
+                animators_weaponSlots[ProjectileControls_Player1.int_weaponIndex_player1].SetInteger
+                    ("WeaponValue",
+                    ProjectileControls_Player1.int_weaponValue_player1);
+                bool_rankUpRequest_player1 = false;
+            }
+            if (bool_beastModeRequest_player1)
+            {
+                animators_weaponSlots[ProjectileControls_Player1.int_weaponIndex_player1].SetInteger
+                    ("WeaponValue",
+                    ProjectileControls_Player1.int_weaponValue_player1);
+                bool_beastModeRequest_player1 = false;
+            }
+            if (bool_restoreWeaponRequest)
+            {
+                animators_weaponSlots[ProjectileControls_Player1.int_weaponIndex_player1].SetInteger
+                    ("WeaponValue",
+                    ProjectileControls_Player1.int_weaponValue_player1);
+                bool_restoreWeaponRequest = false;
+            }
         }
     }
 }
